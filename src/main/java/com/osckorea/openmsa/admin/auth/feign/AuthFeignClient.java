eff80228d22defc7659e1a5298dc60d59389f55a
Ray Lee <ray.lee@osckorea.com>
2023-08-07 10:27:15 +0900

    YAML파일 api-auth url 재작성 및 feign 연결

--- src/main/java/com/osckorea/openmsa/admin/auth/feign/AuthFeignClient.java
+++ src/main/java/com/osckorea/openmsa/admin/auth/feign/AuthFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.osckorea.openmsa.admin.auth.domain.UserSession;
 
-@FeignClient(name = "AUTHENTICATION-CLIENT", url = "${api.auth-token.url}")
+@FeignClient(name = "AUTHENTICATION-CLIENT", url = "${api.auth-service.url}")
 public interface AuthFeignClient {
     @PostMapping("/security/jwtlogin")
     public UserSession.TokenPayload signIn(UserSession.SignInPayload userInfo);
